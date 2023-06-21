5902f2c646b19a0e19ecc36c933bb7728777a301
CroLinux <crolinux@gmail.com>
2023-06-21 21:51:42 +0200

    Simulation with Bank added.

--- PayMyBuddyApp/src/main/java/com/paymybuddy/controller/TransferController.java
+++ PayMyBuddyApp/src/main/java/com/paymybuddy/controller/TransferController.java
@@ -95,4 +95,92 @@ public class TransferController {
 		}
 
 	}
+	
+	@PostMapping("/process_send_to_bank")
+	public String processSendToBank(@ModelAttribute("transaction") Transaction transaction,
+			RedirectAttributes redirectAttributes) {
+
+		User currentUser = userService.currentUser();
+		// List<User> connections = currentUser.getContacts();
+
+		// We need to verify if the user has enough money on his balance
+		if (currentUser.getBalance() < transaction.getAmount()) {
+			//Go back negatively to the page
+			redirectAttributes.addAttribute("errorHigherAmount", "true");
+			return "redirect:/transfer";
+
+		} else {
+
+			// We create and save the new transaction.
+			Transaction newTransaction = new Transaction();
+			newTransaction.setSender_id(currentUser.getId());
+			newTransaction.setReceiver_id(1);
+			newTransaction.setType("Debit");
+			newTransaction.setAmount(transaction.getAmount());
+			newTransaction.setCurrency("EUR");
+			LocalDateTime now = LocalDateTime.now();
+			newTransaction.setDate(now);
+			newTransaction.setDescription("Transfer to my Bank");
+			newTransaction.setFee(0);
+			transactionService.saveTransaction(newTransaction);
+
+			// We need to update the balance of the sender_id
+			float newBalance = currentUser.getBalance() - transaction.getAmount();
+			currentUser.setBalance(newBalance);
+			userService.updateUser(currentUser);
+/**
+			// We need to update the balance of the received_id
+			String receiverEmail = userService.findEmailUserById(transaction.getReceiver_id());
+			User receiverUser = userService.getUserByEmail(receiverEmail);
+			float newReceiverBalance = receiverUser.getBalance() + transaction.getAmount();
+			receiverUser.setBalance(newReceiverBalance);
+			userService.updateUser(receiverUser);
+*/
+			// Go back successfully to the page
+			redirectAttributes.addAttribute("successtobank", "true");
+			return "redirect:/profile";
+
+		}
+
+	}
+	
+	@PostMapping("/process_get_from_bank")
+	public String processGetFromBank(@ModelAttribute("transaction") Transaction transaction,
+			RedirectAttributes redirectAttributes) {
+
+		User currentUser = userService.currentUser();
+		// List<User> connections = currentUser.getContacts();
+
+			// We create and save the new transaction.
+			Transaction newTransaction = new Transaction();
+			newTransaction.setSender_id(1);
+			newTransaction.setReceiver_id(currentUser.getId());
+			newTransaction.setType("Credit");
+			newTransaction.setAmount(transaction.getAmount());
+			newTransaction.setCurrency("EUR");
+			LocalDateTime now = LocalDateTime.now();
+			newTransaction.setDate(now);
+			newTransaction.setDescription("Get from my Bank");
+			newTransaction.setFee(0);
+			transactionService.saveTransaction(newTransaction);
+
+			// We need to update the balance of the receiver_id
+			float newBalance = currentUser.getBalance() + transaction.getAmount();
+			currentUser.setBalance(newBalance);
+			userService.updateUser(currentUser);
+/**
+			// We need to update the balance of the received_id
+			String receiverEmail = userService.findEmailUserById(transaction.getReceiver_id());
+			User receiverUser = userService.getUserByEmail(receiverEmail);
+			float newReceiverBalance = receiverUser.getBalance() + transaction.getAmount();
+			receiverUser.setBalance(newReceiverBalance);
+			userService.updateUser(receiverUser);
+*/
+			// Go back successfully to the page
+			redirectAttributes.addAttribute("successfrombank", "true");
+			return "redirect:/profile";
+
+	
+
+	}
 }
